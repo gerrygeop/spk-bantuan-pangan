dac154a7fc7e47fcb2e8a6030fb35fc7771755b2
gerrygeop <gerryja28@gmail.com>
2021-07-31 06:08:42 +0800

    h2

--- app/views/perhitungan/index.php
+++ app/views/perhitungan/index.php
@@ -3,7 +3,7 @@
     <div class="row align-items-center justify-content-between mb-3 mb-md-0">
         <div class="col-12 col-md-6">
 <<<<<<< HEAD
-            <h3 class="text-secondary">>Hasil Perhitungan</h3>
+            <h2 class="text-secondary">>Hasil Perhitungan</h3>
 =======
             <h2 class="text-secondary">>Hasil Perhitungan</h3>
 >>>>>>> 48abd06aca0fa3a33b1284ed612a1d227e682b8c
